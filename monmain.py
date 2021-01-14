778dfd9d300f10ec616cfcc78091604ecb93dad6
ElianaRenzo <eliana.renzo@epfl.ch>
2021-01-14 16:47:48 +0100

    first commit

---
+++ monmain.py
@@ -0,0 +1,6 @@
+from monoutil import Outil
+print('Hello c est le maiiiinnnn ')
+Out  = Outil(15)
+
+y = Out.carre(3)
+print(y)
---
+++ monoutil.py
@@ -0,0 +1,6 @@
+class Outil:
+    def __init__(self, prix):
+        self.prix = prix
+
+    def carre(self, x):
+        return x**2
